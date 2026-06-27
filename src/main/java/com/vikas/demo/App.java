e6a40757d1720fddb5c78b702babe4bea88ce6b3
Jagmohan Sharma <jagmohansharma202@gmail.com>
2026-06-27 12:32:42 +0530

    Initial Maven Commit

---
+++ src/main/java/com/vikas/demo/App.java
@@ -0,0 +1,13 @@
+package com.vikas.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello, Devops world!");
+        System.out.println("This is our project");
+    }
+}
