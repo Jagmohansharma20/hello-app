78e7f172f110ceaf9857e52964970a2e4f102092
Jagmohan Sharma <jagmohansharma202@gmail.com>
2026-06-30 20:03:07 +0530

    webhook testing again

--- src/main/java/com/vikas/demo/App.java
+++ src/main/java/com/vikas/demo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main(String[] args) {
         System.out.println("Hello, Devops world!");
         System.out.println("This is our project");
-        System.out.println("This is web hook testing");
+        System.out.println("This is web hook testings");
     }
 }
